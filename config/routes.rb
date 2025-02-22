f95f78a9fa8592011b8200e0e69b2d0933e2e6cb
shashwatikulkarni <136837540+shashwatikulkarni@users.noreply.github.com>
2025-02-22 17:34:53 +0530

    changes in about section

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  get "pages/about"
   resources :recipes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: redirect("/recipes")
